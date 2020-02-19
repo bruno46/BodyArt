c592cd2ad3a1d24868dafe1f59f6532915f56531
yayistark <yayistark22@outlook.es>
2020-02-19 18:44:51 -0300

    Revert "se agrego carpeta clases/"
    This reverts commit ba6b23ddcd850d74eddaecf49cdb13d3581b3f5b.

--- clases/Db.php
+++
@@ -1,7 +0,0 @@
-<?php
-abstract class Db
-{
-  public abstract function guardarUsuario(Usuario $usuario);
-  public abstract function traerTodos();
-  public abstract function traerPorMail($email);
-}
