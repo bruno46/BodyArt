899c1b838bd4a0c6d73fef7baca0bb4d92463a3d
yayistark <yayistark22@outlook.es>
2020-02-20 01:17:16 -0300

    se agrego pdo.php / soporte.php

---
+++ soporte.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'pdo.php';
+include_once 'clases/Validador.php';
+include_once 'clases/Auth.php';
+include_once 'clases/Usuario.php';
+include_once 'clases/Db.php';
+include_once 'clases/DbJson.php';
+include_once 'clases/DbMySQL.php';
+
+
+$dbType = "mysql";
+
+switch ($dbType) {
+  case 'mysql':
+    $db = new DbMySQL();
+    break;
+
+  case 'json':
+    $db = new DbJson();
+    break;
+}
+$auth = new Auth();
+$validador = new Validador();
